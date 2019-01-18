8cb0f6c1235465edf455b023d42c6189239e7db7
parkerlreed <parker.l.reed@gmail.com>
2019-01-18 17:44:21 -0500

    Update ical2vcal.py

--- ical2vcal.py
+++ ical2vcal.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python
 
+# ical2vcal.cgi by Charl P. Botha http://cpbotha.net/
+# based on http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/273844
+# integrated work by Mark Graves for VCS file downloading (much better
+# than copying and pasting!)
+
 import os, sys
 import vcal
 
